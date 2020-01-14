46be1c2383acf8c3f4cc82b9a81a1fe8298fb1bf
Antonio Rodrigues <arodriguez245@cnm.edu>
2020-01-13 22:11:05 -0700

    Add project files.

---
+++ EmployeeData2/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeData2.Models
+{
+    public class Employees
+    {
+        public int EmployeeNum { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+
+    }
+}
